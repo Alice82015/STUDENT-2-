fb593cbbd6d55f4ae01bd69d45cd80a304f04a2b
m305-9\w401 <a106082015@mail.shu.edu.tw>
2018-03-29 17:06:22 +0800

    (first)第一二節課堂練習

---
+++ STUDENT/STUDENT/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = { "葉俊廷", "張澤瑜", "王程捷 的咩？", "江儀婷", "張秉華", "陳柏霖", "黃紀瑜", "黃昱維", "蔡逸群", "登琳", "世新魏鈞孝吧", "劉定南", "陳信如", "童信傑", "李岳倫", "鄒和恆", "簡毓玟", "劉子瑄", "蕭紹洋", "李亞宸la", "張以潔", "邱仕紳", "呂家瑩", "世新林鑫佑", "徐均得", "陳佳欣", "張恩瑋", "周詮", "王湘婷", "蕭宇成", "Max Cheung(BANG)", "馬嘉誠Σヽ(ﾟД ﾟ; )ﾉ魔貫光殺砲(ﾟДﾟ)σ━0000", "陳昱嘉", "康珈熏", "盧奕宏", "陳郁佳的大哥黃博涵", "遲正雯", "世新林星彤", "許晏誠ｘ勒是奇多＿８＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝Ｄ　", "許子安", "高子晴", "陳思婷", "矝鵘鮿", "余彥廷", "吳耀輝", "鄭丞智", "林冠廷", "世新何曜宇", "邵喬雨", "張豐愷", "林子晴", "邱雁回", "陳家欣是韓妞", "鄧雅馨", "謝汶珊", "李曼寧", "世新徐偉哲", "张钰慈", "鍾宜珊", "raer_tsai", "世新李姳諼", "鄭曼君", "陳柏霖", "世新許子安", "世新方若帆", "李曼寧", "世新遲正雯", "世新周詮" };
+            string[] bloodTypes = { "O", "其他", "O", "O", "A", "O", "A", "A", "O", "O", "A", "O", "A", "B", "O", "O", "A", "其他", "O", "O", "A", "", "B", "O", "", "O", "B", "O", "B", "B", "B", "O", "O", "AB", "O", "B", "A", "O", "O", "", "O", "A", "", "O", "O", "A", "O", "O", "其他", "B", "O", "O", "O", "A", "AB", "A", "O", "B", "AB", "", "O", "O", "O", "", "O", "A", "A", "O" };
+            int[] heights = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 167, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 165, 155, 0, 168, 160, 169, 0, 150, 163, 168, 173 };
+            string[] genders = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("學生名單:" + names[i]);
+            }
+
+
+            double count = 0,count2 = 0,count3 = 0,count4  = 0;
+            for(int u = 0; u < bloodTypes.Length; u++)
+            {
+                if (bloodTypes[u] == "A")
+                {
+                    count++;
+                }
+                if (bloodTypes[u] == "B")
+                {
+                    count2++;
+                }
+
+            }
+
+            double precentA = count / (double)bloodTypes.Length * 100;
+            double precentB = count / (double)bloodTypes.Length * 100;
+
+            // 顯示該血型總人數與百分比
+            Console.WriteLine("A型的人有 {0}  {1:0}%",count,precentA);
+            Console.WriteLine("B型的人有 {0}  {1:0}%", count2, precentB);
+
+
+
+
+
+
+            int sum = 0;
+            int Count = 0;
+
+            for (int u = 0; u < heights.Length; u++)
+            {
+                if(heights[u]>100 && heights[u]<200)
+                {
+                    if(genders[u] == "女")
+                    {
+                        sum = sum + heights[u];
+                        Count++;
+                    }
+                }
+            }
+
+            double avg = sum / Count;
+
+            Console.WriteLine("女身高總=" + sum);
+            Console.WriteLine("女身高平均:" + avg);
+
+            Console.ReadLine();
+        
+
+               
+
+
+        }
+    }
+}
